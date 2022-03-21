427277997a22383f9aff55f419d0d0a17c426de1
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2022-03-20 21:38:08 -0700

    Parse app info from config.xml

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Firebase App Init
 
-This plugin sets up an additional firebase app for a cordova app.
+This plugin sets up an additional firebase app for a Cordova app.
 
 ## Setup
 
-- Clone the repo
-- Modify FirebaseAppInit.java to configure a secondary firebase app
-- Install the plugin
+1. Clone the plugin (not yet released on NPM)
 
- The firebase app will be created under the "secondary" name.
+2. Install this plugin using Cordova CLI:
+
+    npx cordova plugin add ../path/to/plugin
+
+3. Configure the plugin by adding the firebase app info to the app's config.xml:
+
+<preference name="com.airship.firebase.project_id" value="string:<PROJECT_NAME>" />
+<preference name="com.airship.firebase.application_id" value="string:<APPLICATION_ID>" />
+<preference name="com.airship.firebase.api_key" value="string:<API_KEY>" />
+<preference name="com.airship.firebase.sender_id" value="string:<SENDER_ID>" />
+<preference name="com.airship.firebase.app_name" value="string:<APP_NAME>" />
+
--- src/android/FirebaseAppInit.java
+++ src/android/FirebaseAppInit.java
@@ -3,37 +3,61 @@
 package com.airship.cordova;
 
 import android.content.Context;
+import android.content.res.XmlResourceParser;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.util.ObjectsCompat;
+import androidx.startup.Initializer;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.startup.Initializer;
+import java.util.Locale;
+import java.util.Map;
 
 public class FirebaseAppInit implements Initializer<String> {
 
-    private static final String APP_NAME = "secondary";
+    private static final String CONFIG_PREFIX = "com.airship.firebase.";
+    private static final String PROJECT_ID = CONFIG_PREFIX + "project_id";
+    private static final String APPLICATION_ID = CONFIG_PREFIX + "application_id";
+    private static final String API_KEY = CONFIG_PREFIX + "api_key";
+    private static final String SENDER_ID = CONFIG_PREFIX + "sender_id";
+    private static final String APP_NAME = CONFIG_PREFIX + "app_name";
+
+    private static final String STRING_PREFIX = "string:";
 
     @NonNull
     @Override
     public String create(@NonNull Context context) {
-        // Manually configure Firebase Options. The following fields are REQUIRED:
-        //   - Project ID
-        //   - App ID
-        //   - API Key
-        //   - Sender ID
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setProjectId("PROJECT_ID")
-                .setApplicationId("APPLICATION_ID")
-                .setApiKey("API_KEY")
-                .setGcmSenderId("SENDER ID")
-                .build();
+        try {
+            Map<String, String> config = parseConfigXml(context);
 
-        FirebaseApp.initializeApp(context, options, APP_NAME);
-        return APP_NAME;
+            // Required
+            String projectId = ObjectsCompat.requireNonNull(config.get(PROJECT_ID));
+            String applicationId = ObjectsCompat.requireNonNull(config.get(APPLICATION_ID));
+            String apiKey = ObjectsCompat.requireNonNull(config.get(API_KEY));
+            String appName = ObjectsCompat.requireNonNull(config.get(APP_NAME));
+
+            String senderId = config.get(SENDER_ID);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setProjectId(projectId)
+                    .setApplicationId(applicationId)
+                    .setApiKey(apiKey)
+                    .setGcmSenderId(senderId)
+                    .build();
+
+            FirebaseApp.initializeApp(context, options, appName);
+            return appName;
+        } catch (Exception e) {
+            Log.e("FirebaseAppInit", "Failed to create Firebase app", e);
+            return "";
+        }
     }
 
     @NonNull
@@ -41,4 +65,53 @@ public class FirebaseAppInit implements Initializer<String> {
     public List<Class<? extends Initializer<?>>> dependencies() {
         return Collections.emptyList();
     }
+
+
+    @NonNull
+    public static Map<String, String> parseConfigXml(@NonNull Context context) {
+        Map<String, String> config = new HashMap<String, String>();
+        int id = context.getResources().getIdentifier("config", "xml", context.getPackageName());
+        if (id == 0) {
+            return config;
+        }
+
+        XmlResourceParser xml = context.getResources().getXml(id);
+
+        int eventType = -1;
+        while (eventType != XmlResourceParser.END_DOCUMENT) {
+
+            if (eventType == XmlResourceParser.START_TAG) {
+                if (xml.getName().equals("preference")) {
+
+                    String name = xml.getAttributeValue(null, "name").toLowerCase(Locale.US);
+                    if (name.startsWith(CONFIG_PREFIX)) {
+                        String value = parseValue(xml.getAttributeValue(null, "value"));
+                        if (value != null) {
+                            config.put(name, value);
+                        }
+                    }
+                }
+            }
+
+            try {
+                eventType = xml.next();
+            } catch (Exception e) {
+                Log.e("FirebaseAppInit", "Failed to parse config", e);
+            }
+        }
+
+        return config;
+    }
+
+    public static String parseValue(@Nullable String value) {
+        if (value == null) {
+            return null;
+        }
+
+        if (value.startsWith(STRING_PREFIX)) {
+            return value.substring(STRING_PREFIX.length());
+        }
+        return value;
+    }
 }
+
